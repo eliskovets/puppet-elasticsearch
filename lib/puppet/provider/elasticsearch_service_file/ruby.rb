5c8ffca6a13756174506cab955ac4843068b32f8
Evgeny Liskovets <eugene.liskovets@gmail.com>
2018-07-06 13:18:29 +0200

    Fix for op_flags

--- lib/puppet/provider/elasticsearch_service_file/ruby.rb
+++ lib/puppet/provider/elasticsearch_service_file/ruby.rb
@@ -67,6 +67,10 @@ Puppet::Type.type(:elasticsearch_service_file).provide(:ruby) do
     opt_flag, opt_flags = Puppet_X::Elastic::EsVersioning.opt_flags(
       resource[:package_name], resource.catalog
     )
+
+    # This should only be present on systemd systems.
+    opt_flags.delete('--quiet') unless resource[:name].include?('systemd')
+
     template = ERB.new(resource[:content], 0, "-")
     result = template.result(binding)
 
